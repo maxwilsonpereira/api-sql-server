d12d313f0171d4b62ca6c2a2401a7c7ba3daeb77
maxwilsonpereira <maxwilsonpereira@gmail.com>
2021-12-14 22:34:12 +0100

    backend will connect to heroku sql db

--- dist/dbCon.js
+++ dist/dbCon.js
@@ -5,15 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2")); // npm i mysql2
 const dbCon = mysql2_1.default.createConnection({
-    host: process.env.host,
+    database: process.env.database,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database,
+    host: process.env.host,
 });
-console.log('host: ', process.env.host);
+console.log('database: ', process.env.database);
 console.log('user: ', process.env.user);
 console.log('password: ', process.env.password);
-console.log('database: ', process.env.database);
+console.log('host: ', process.env.host);
 dbCon.connect(function (err) {
     if (err) {
         return console.error('error: ' + err.message);
--- src/dbCon.ts
+++ src/dbCon.ts
@@ -1,16 +1,16 @@
 import mysql from 'mysql2'; // npm i mysql2
 
 const dbCon = mysql.createConnection({
-  host: process.env.host,
+  database: process.env.database,
   user: process.env.user,
   password: process.env.password,
-  database: process.env.database,
+  host: process.env.host,
 });
 
-console.log('host: ', process.env.host);
+console.log('database: ', process.env.database);
 console.log('user: ', process.env.user);
 console.log('password: ', process.env.password);
-console.log('database: ', process.env.database);
+console.log('host: ', process.env.host);
 dbCon.connect(function (err) {
   if (err) {
     return console.error('error: ' + err.message);
